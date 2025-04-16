3db9e477509cff76569342583639fc7da9fd4386
BeeEye Dmu <bikang@konyang.ac.kr>
2025-04-16 15:51:33 +0900

    Add files via upload

---
+++ 027_Student/Program.cs
@@ -0,0 +1,34 @@
+﻿namespace _027_Student
+{
+  class Student
+  {
+    private string name;
+    private string major;
+    private int age;
+
+    public Student(string name, string major, int age)
+    {
+      this.name = name;
+      this.major = major;
+      this.age = age;
+    }
+
+    public void DusplayInfo()
+    {
+      //Console.WriteLine("이름: {0}\n전공: {1}\n나이: {2}\n",
+      // name, major, age);
+      Console.WriteLine($"이름: {name}\n전공: {major}\n나이: {age}\n");
+    }
+  }
+
+
+  internal class Program
+  {
+    static void Main(string[] args)
+    {
+      Student s = new Student("홍길동", "의료IT공학", 20);
+
+      s.DusplayInfo();
+    }
+  }
+}
---
+++ 028_calculator/Program.cs
@@ -0,0 +1,43 @@
+﻿namespace _028_calculator
+{
+  class Calulator
+  {
+    public static int Add(int x, int y)
+    {
+      return x + y;
+    }
+    public static int Subtract(int x, int y)
+    {
+      return x - y;
+    }
+    public static int Multiply(int x, int y)
+    {
+      return x * y;
+    }
+    public static double Divide(int x, int y)
+    {
+      if (y == 0)
+      {
+        Console.WriteLine("0으로는 나눌 수 없습니다");
+        return 0;
+      }
+      else
+        return (double)x / y;
+    }
+  }
+  internal class Program
+  {
+    static void Main(string[] args)
+    {
+      int x = 10;
+      int y = 20;
+
+      int sum = Calulator.Add(x, y);  // 스태틱 매소드 호출
+      int diff = Calulator.Subtract(x, y);
+      int prod = Calulator.Multiply(x, y);
+      double quot = Calulator.Divide(x, y);
+
+      Console.WriteLine($"덧셈: {sum}\n뺄셈: {diff}\n곱셈: {prod}\n나눗셈: {quot}");
+    }
+  }
+}
